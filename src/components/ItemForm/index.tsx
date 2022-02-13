f4985e343eec39539b24637bc52f003d05a1ae5e
Shimpei Takamatsu <shimpeiws@gmail.com>
2022-02-14 06:56:35 +0900

    Introduce react-hook-form

---
+++ src/components/ItemForm/index.tsx
@@ -0,0 +1,23 @@
+import { useForm } from "react-hook-form";
+
+export const ItemForm = (): ReactElement => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p>Item/New</p>
+      <input defaultValue="test" {...register("example")} />
+      <input {...register("exampleRequired", { required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
+  );
+};
