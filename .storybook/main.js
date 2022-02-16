0bd8ad6413135f55bc1d612158e13d5fda5cbe31
Shimpei Takamatsu <shimpeiws@gmail.com>
2022-02-17 06:05:34 +0900

    Introduce storybook

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  },
+  core: {
+    builder: 'webpack5',
+  },
+}
