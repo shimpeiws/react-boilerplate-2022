c018e1b64e8328d45bd440c76d02122ba2cbd5ed
Shimpei Takamatsu <shimpeiws@gmail.com>
2022-02-21 08:56:32 +0900

    Fix component name

--- src/pages/item/new.tsx
+++ src/pages/item/new.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ItemForm } from "../../components/ItemForm";
 import { Header } from "../../components/header";
 
-export default function Index() {
+export default function New() {
   return (
     <div>
       <Header />
