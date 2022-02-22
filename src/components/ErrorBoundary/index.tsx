cdc0231df216de755436199fec473662c387127b
Shimpei Takamatsu <shimpeiws@gmail.com>
2022-02-23 06:45:54 +0900

    Error boundary

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // TODO: Send error to tracking tool
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import { RecoilRoot } from "recoil";
+import { ErrorBoundary } from "../components/ErrorBoundary";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <p>React Boilerplate 2022</p>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <ErrorBoundary>
+      <RecoilRoot>
+        <p>React Boilerplate 2022</p>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ErrorBoundary>
   );
 }
